7098147af1f3aba32bc8e13b3d57c10435f2aaef
Yoori Choi <yoori.choi1@gmail.com>
2021-03-26 11:52:50 -0400

    Initial style for config-languages

--- client/src/components/ConfigLanguage/ConfigLanguage.jsx
+++ client/src/components/ConfigLanguage/ConfigLanguage.jsx
@@ -6,23 +6,22 @@ function ConfigLanguage() {
   return (
     <div>
       <Setup />
-      <h2>My preferred language is</h2>
-      <label htmlFor="languages">My preferred language is</label>
+      <div className="content-1">
+        <h2 className="content-1__title">My preferred language is</h2>
+        <label htmlFor="languages">My preferred language is</label>
+        <select name="languages" id="languages">
+          <option value="english">My preferred language is</option>
+          <option value="spanish">mi idioma preferido es</option>
+          <option value="chinese">我首选的语言是</option>
+          <option value="french">ma langue préférée est</option>
+          <option value="hindi">मेरी पसंदीदा भाषा है</option>
+        </select>
 
-      <select name="languages" id="languages">
-        <option value="english">My preferred language is</option>
-        <option value="spanish">mi idioma preferido es</option>
-        <option value="chinese">我首选的语言是</option>
-        <option value="french">ma langue préférée est</option>
-        <option value="hindi">मेरी पसंदीदा भाषा है</option>
-      </select>
-      
-      <label htmlFor="name">
-        Full Legal Name: 
-      </label>
+        <label htmlFor="name">Full Legal Name:</label>
+      </div>
       <NextButton link="/config-2-info" />
     </div>
-  )
+  );
 }
 
 export default ConfigLanguage;
